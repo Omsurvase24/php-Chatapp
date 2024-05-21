190c95fbb3e105448c41c6ac6722b0b52afdba2a
Omsurvase24 <omsurvase24@gmail.com>
2024-05-21 23:38:29 +0530

    dbconnection set

--- app/helpers/conversations.php
+++ app/helpers/conversations.php
@@ -1,52 +1,39 @@
-<?php 
+<?php
 
-function getConversation($user_id, $conn){
-    /**
-      Getting all the conversations 
-      for current (logged in) user
-    **/
-    $sql = "SELECT * FROM conversations
+function getConversation($user_id, $conn)
+{
+  $sql = "SELECT * FROM conversations
             WHERE user_1=? OR user_2=?
             ORDER BY conversation_id DESC";
 
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$user_id, $user_id]);
+  $stmt = $conn->prepare($sql);
+  $stmt->execute([$user_id, $user_id]);
 
-    if($stmt->rowCount() > 0){
-        $conversations = $stmt->fetchAll();
+  if ($stmt->rowCount() > 0) {
+    $conversations = $stmt->fetchAll();
 
-        /**
-          creating empty array to 
-          store the user conversation
-        **/
-        $user_data = [];
-        
-        # looping through the conversations
-        foreach($conversations as $conversation){
-            # if conversations user_1 row equal to user_id
-            if ($conversation['user_1'] == $user_id) {
-            	$sql2  = "SELECT *
+    $user_data = [];
+
+    foreach ($conversations as $conversation) {
+      if ($conversation['user_1'] == $user_id) {
+        $sql2  = "SELECT *
             	          FROM users WHERE user_id=?";
-            	$stmt2 = $conn->prepare($sql2);
-            	$stmt2->execute([$conversation['user_2']]);
-            }else {
-            	$sql2  = "SELECT *
+        $stmt2 = $conn->prepare($sql2);
+        $stmt2->execute([$conversation['user_2']]);
+      } else {
+        $sql2  = "SELECT *
             	          FROM users WHERE user_id=?";
-            	$stmt2 = $conn->prepare($sql2);
-            	$stmt2->execute([$conversation['user_1']]);
-            }
+        $stmt2 = $conn->prepare($sql2);
+        $stmt2->execute([$conversation['user_1']]);
+      }
 
-            $allConversations = $stmt2->fetchAll();
+      $allConversations = $stmt2->fetchAll();
+      array_push($user_data, $allConversations[0]);
+    }
 
-            # pushing the data into the array 
-            array_push($user_data, $allConversations[0]);
-        }
-
-        return $user_data;
-
-    }else {
-    	$conversations = [];
-    	return $conversations;
-    }  
-
+    return $user_data;
+  } else {
+    $conversations = [];
+    return $conversations;
+  }
 }
