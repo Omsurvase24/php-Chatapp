7dca1ae4210ea9ac09e52308b0253e5c8475ff1b
Omsurvase24 <omsurvase24@gmail.com>
2024-05-22 20:14:35 +0530

    logo

--- chat.php
+++ chat.php
@@ -35,7 +35,7 @@ if (isset($_SESSION['username'])) {
 		<title>Chat App</title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/style.css">
-		<link rel="icon" href="img/logo.png">
+		<link rel="icon" href="images/logo.png">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 
